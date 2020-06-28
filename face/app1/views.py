90e559cbe13239c31b8de7553fbd4ba108e945b0
archit-soni <43818375+archit-soni@users.noreply.github.com>
2020-06-28 13:21:28 -0600

    more

--- face/app1/views.py
+++ face/app1/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from random import randint
+from app1.models import dataBase
+
 
 
 def index(request):
+	
 	
 	a = randint(1, 94)
 	b = randint(1, 94)
@@ -14,4 +17,61 @@ def index(request):
 		"a":filepath,
 		"b":filepath2
 	}
+
+	left = dataBase.objects.get(id=a)
+	right = dataBase.objects.get(id=b)
+	left.selected = left.selected+1
+	right.selected = right.selected+1
+
+
+	left.save()
+	right.save()
+
+	winner_number = 1
+	winner =''
+	loser=''
+	if 'first' in request.POST:
+		winner='first'
+		winner_number=request.POST['first']
+		winner_number=winner_number[winner_number.index('c')+2:winner_number.index('.')]
+		loser='second' 
+	elif 'second' in request.POST:
+		winner='second'
+		winner_number=request.POST['second']
+		winner_number=winner_number[winner_number.index('c')+2:winner_number.index('.')]
+		loser='first'
+
+
+	winner = dataBase.objects.get(id=winner_number)
+	winner.voted=winner.voted+1
+	
+	winner.save()
+
+		
 	return render(request,'home.html',args)
+
+def rankings(request):
+
+	sortedList = []
+	data = {}
+	for i in range(1, 96):
+		item = dataBase.objects.get(id=i)
+		if item.selected == 0:
+			item.votePer = 0.0
+		else:
+			item.votePer = item.voted/item.selected
+		data[i]=item.votePer
+	print(data)
+	sortedList = sorted(data.items() , key=lambda t : t[1], reverse=True)
+	args = {}
+	temp=[]
+	for i in sortedList[:10]:
+		temp.append(i[0])
+	for i in range(len(temp)):
+		temp[i]="/static/"+str(temp[i])+".jpg"
+
+	args["data"]=temp	
+	print(args)
+
+	return render(request, 'ranking.html', args)
+	
--- face/face/settings.py
+++ face/face/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,12 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app1.apps.App1Config',
 ]
 
 MIDDLEWARE = [
@@ -130,4 +133,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+django_heroku.settings(local())
 
